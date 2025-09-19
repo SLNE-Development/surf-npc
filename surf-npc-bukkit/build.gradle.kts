dfec9783a6094e0268d7fa1a1fd4114f32737f93
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-19 15:26:12 +0200

    chore: remove version declaration from build script

--- surf-npc-bukkit/build.gradle.kts
+++ surf-npc-bukkit/build.gradle.kts
@@ -6,8 +6,6 @@ dependencies {
     api(project(":surf-npc-core"))
 }
 
-version = "1.21.7-1.0.0-SNAPSHOT"
-
 surfPaperPluginApi {
     mainClass("dev.slne.surf.npc.bukkit.BukkitMain")
     authors.add("red")
