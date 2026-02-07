bb3a50863863dfd86f9862e1ce422700e3146860
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-07 14:24:00 +0100

    feat: Update NPC skin handling and streamline NPC creation in example plugin

--- surf-npc-api/src/main/kotlin/dev/slne/surf/npc/api/npc/skin/NpcSkin.kt
+++ surf-npc-api/src/main/kotlin/dev/slne/surf/npc/api/npc/skin/NpcSkin.kt
@@ -1,6 +1,6 @@
 package dev.slne.surf.npc.api.npc.skin
 
-import dev.slne.surf.surfapi.core.api.util.mutableObjectSetOf
+import dev.slne.surf.surfapi.core.api.util.toObjectSet
 import it.unimi.dsi.fastutil.objects.ObjectSet
 
 data class NpcSkin(
@@ -23,10 +23,10 @@ data class NpcSkin(
 
     companion object {
         fun empty() = NpcSkin(
-            ownerName = "",
-            value = "",
-            signature = "",
-            parts = mutableObjectSetOf()
+            "default",
+            "ewogICJ0aW1lc3RhbXAiIDogMTY3NDQxNzAzMzQ2MCwKICAicHJvZmlsZUlkIiA6ICIzY2MxMTY3MWU0MTM0ODM0YjhjMmZjMTY1OGE4OWU3OCIsCiAgInByb2ZpbGVOYW1lIiA6ICJSb3NzaTU2IiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2QwZGEwZjE0YTg1YjQzODNjYWFiNGM2NWJlOTBkMzMyNWEzMGIwY2UwNGI2YzA4ODQzODcxMDIwNDkwZDk0N2IiLAogICAgICAibWV0YWRhdGEiIDogewogICAgICAgICJtb2RlbCIgOiAic2xpbSIKICAgICAgfQogICAgfQogIH0KfQ==",
+            "VjpajnLZ4dIdfrXQJFFyV7Fp/IKEcQmEZIbiVR3MtFalVQ8BS5wCv8dsQeTmwZbv4zo4lL1urA4boAC9NIcP8KK+ucg19kgGrNDTs3Gmo7j57kZipDqKHJZMaHcvGx2Vur61YNL+cLQ9kwHaPwWdEdd9SDCJuXpvlBLrIxM8bqacyE1S/aATU388373xg2rNPCOxUy4YhScqeDSkBYdlYJ6xqQTwVIZZ8iwE/rqjq/X66VMRL6EtwwOqGqUBusvIiAfnGsyqMJ4rtohRsF9YHOR4cIk8K6E12ryl8uyp/HAkOYvfuk7TQuQSZpfgkH0aObjAeFPLGGGbV+P1cyezZm0I3erOQhviN9zfeaiuTHBmfP+RSb6dw+eQVKkdE2GkNinRp5EX5pltR3wREos2Muh2LXw3gGgWR3HvKnAZ1ofgAcIGk6pq9IKTpGaRZYhjIMpMXGsiRlgYqN4BmEDhTlV1XIldpatju6QnbTYyr8oJ97Z5FEVkhQ+5GsQnU9Rx+GyANhJjGVU06WiS/3H42SxbKaoPPIbBGPN3H8JTWO5Av3Y2zYLhWLhPFKF0b09FbhmkMJq0W9qE82DoByY/mhRmwTb1yrbKwXwxScEC4CSvbQBxwNjtp2WwttYs3L/x4Sxf4PgkPL0lvUF+yt+OGkHgJw4EzxV2giJaLfsXmsQ=",
+            NpcSkinPart.entries.toObjectSet()
         )
     }
 }
--- surf-npc-example-dsl/src/main/kotlin/dev/slne/surf/npc/example/SurfNpcExamplePlugin.kt
+++ surf-npc-example-dsl/src/main/kotlin/dev/slne/surf/npc/example/SurfNpcExamplePlugin.kt
@@ -23,7 +23,7 @@ class SurfNpcExamplePlugin() : SuspendingJavaPlugin() {
         /**
          * Creates an example NPC using the DSL provided by the Surf-NPC API.
          */
-        npc(this) {
+        npc {
             displayName {
                 append(
                     MiniMessage.miniMessage()
@@ -85,11 +85,7 @@ class SurfNpcExamplePlugin() : SuspendingJavaPlugin() {
         surfNpcApi.addProperty(npc, npcProperty {
             key = "example_npc"
             value = true
-            type = surfNpcApi.getPropertyType(NpcPropertyType.Types.BOOLEAN_ID)
-                ?: return@npcProperty run {
-                    logger().atWarning()
-                        .log("Failed to create example NPC: Boolean property type not found.")
-                }
+            type = NpcPropertyType.Types.BOOLEAN_TYPE
         })
     }
 }
